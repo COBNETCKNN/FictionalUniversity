c29dbeb9f92558c010b204296446ffc48efdedf8
COBNETCKNN <ahmedmeshkin@gmail.com>
2020-11-21 00:05:11 +0100

    dynamic slider

--- front-page.php
+++ front-page.php
@@ -132,39 +132,38 @@
       </div>
     </div>
 
-    <div class="hero-slider">
-      <div data-glide-el="track" class="glide__track">
-        <div class="glide__slides">
-          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bus.jpg')?>);">
+    
+
+
+    <?php if( have_rows('slider') ): ?>
+
+    
+      <div class="hero-slider">
+      <div class="slider__bullets glide__bullets" data-glide-el="controls[nav]"></div>
+        <div data-glide-el="track" class="glide__track">
+          <div class="glide__slides">
+            
+
+    <?php while( have_rows('slider') ): the_row(); 
+        $image = get_sub_field('picture');
+        ?>
+      
+          <div class="hero-slider__slide" style="background-image: url(<?php echo $image?>);">
             <div class="hero-slider__interior container">
               <div class="hero-slider__overlay">
-                <h2 class="headline headline--medium t-center">Free Transportation</h2>
-                <p class="t-center">All students have free unlimited bus fare.</p>
+                <h2 class="headline headline--medium t-center"><?php echo get_sub_field('title'); ?></h2>
+                <p class="t-center"><?php echo get_sub_field('subtitle'); ?></p>
                 <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
               </div>
             </div>
           </div>
-          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('images/apples.jpg')?>);">
-            <div class="hero-slider__interior container">
-              <div class="hero-slider__overlay">
-                <h2 class="headline headline--medium t-center">An Apple a Day</h2>
-                <p class="t-center">Our dentistry program recommends eating apples.</p>
-                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
-              </div>
-            </div>
-          </div>
-          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('images/bread.jpg')?>);">
-            <div class="hero-slider__interior container">
-              <div class="hero-slider__overlay">
-                <h2 class="headline headline--medium t-center">Free Food</h2>
-                <p class="t-center">Fictional University offers lunch plans for those in need.</p>
-                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
-              </div>
-            </div>
-          </div>
+        
+    <?php endwhile; ?>
+
         </div>
-        <div class="slider__bullets glide__bullets" data-glide-el="controls[nav]"></div>
       </div>
     </div>
+
+<?php endif; ?>
 
 <?php get_footer(); ?>
